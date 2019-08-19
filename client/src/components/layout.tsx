28261f8d9625ad860294163203b25d21d149c651
Emanuel Kluge <mail@emanuel-kluge.de>
2019-08-19 21:07:00 +0200

    fix(client): make logout request pass the API gateway

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -6,7 +6,9 @@ export const Layout: React.SFC = ({ children }) => (
   <>
     <header className={styles.header}>
       <form method="post" action="/logout" className={styles.form}>
-        <button className="nes-btn">Logout</button>
+        <button className="nes-btn" name="logout">
+          Logout
+        </button>
       </form>
     </header>
     <main className={styles.main}>{children}</main>
