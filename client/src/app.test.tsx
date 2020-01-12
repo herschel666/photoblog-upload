ac31ba9dde4c74828d7879f60b960be21ed81686
Emanuel Kluge <mail@emanuel-kluge.de>
2020-01-12 13:56:57 +0100

    test(client): test upload happy path

--- client/src/app.test.tsx
+++ client/src/app.test.tsx
@@ -1,8 +1,44 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, waitForElement, act } from '@testing-library/react';
 
+import { imageFile, createApiScope, createUploadScope } from './test-helper';
 import App from './app';
 
-it('renders without crashing', () => {
-  render(<App />);
+const timeout = async (ms: number) =>
+  new Promise((resolve: () => void) => setTimeout(resolve, ms));
+
+it('uploads an image', async () => {
+  const { getByText, getByLabelText, getByTestId } = render(<App />);
+  const submit = getByText('Submit') as HTMLButtonElement;
+  const input = getByLabelText('File') as HTMLInputElement;
+
+  Object.defineProperty(input, 'files', {
+    get: () => [imageFile],
+  });
+  createApiScope();
+  createUploadScope();
+
+  fireEvent.change(input);
+  fireEvent.change(getByLabelText('Title'), {
+    target: { value: 'Test' },
+  });
+  fireEvent.change(getByLabelText('Tags'), {
+    target: { value: 'lorem, ipsum, dolor' },
+  });
+  fireEvent.change(getByLabelText('Alt-Text'), {
+    target: { value: 'Nothing to see here.' },
+  });
+  fireEvent.change(getByLabelText('Description'), {
+    target: { value: 'void' },
+  });
+  await act(async () => void (await timeout(10)));
+
+  expect(submit.disabled).toBe(false);
+
+  fireEvent.click(submit);
+
+  await waitForElement(() => getByTestId('progress'));
+  await act(async () => void (await timeout(200)));
+
+  expect(() => getByTestId('progress')).toThrow();
 });
--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,4 +1,10 @@
-global.photoblogUploadClient = {
+window.photoblogUploadClient = {
   SPACE_ID: 'space_id',
   ACCESS_TOKEN: 'access_token',
+  CF_ENV: 'staging',
 };
+
+Object.defineProperty(window, 'scrollTo', {
+  value: () => void 0,
+  writable: true,
+});
