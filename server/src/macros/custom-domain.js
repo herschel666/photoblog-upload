d933c404139155aa07392fa9bb40a176e3e99006
Emanuel Kluge <mail@emanuel-kluge.de>
2020-09-21 21:03:01 +0200

    fix: update custom domain settings
    ...in order to be compatible with "AWS::Serverless::HttpApi"

--- server/src/macros/custom-domain.js
+++ server/src/macros/custom-domain.js
@@ -1,5 +1,3 @@
-const API_GATEWAY_ZONE_ID = 'Z2FDTNDATAQYW2';
-
 module.exports = (_, cfn, stage) => {
   cfn.Parameters = {
     ...cfn.Parameters,
@@ -20,42 +18,13 @@ module.exports = (_, cfn, stage) => {
       Default: process.env.CERTIFICATE_ARN,
     },
   };
-
-  cfn.Resources.CustomDomainName = {
-    Type: 'AWS::ApiGateway::DomainName',
-    Properties: {
-      DomainName: { Ref: 'DomainName' },
-      CertificateArn: { Ref: 'CertificateArn' },
-      EndpointConfiguration: {
-        Types: ['EDGE'],
-      },
-    },
-  };
-  cfn.Resources.DomainRecordSet = {
-    Type: 'AWS::Route53::RecordSet',
-    Properties: {
-      Name: { Ref: 'DomainName' },
+  cfn.Resources.HTTP.Properties.Domain = {
+    CertificateArn: { Ref: 'CertificateArn' },
+    DomainName: { Ref: 'DomainName' },
+    Route53: {
       HostedZoneId: { Ref: 'Zone' },
-      Type: 'A',
-      AliasTarget: {
-        DNSName: {
-          'Fn::GetAtt': ['CustomDomainName', 'DistributionDomainName'],
-        },
-        HostedZoneId: API_GATEWAY_ZONE_ID,
-      },
     },
   };
-  cfn.Resources.BasePathMapping = {
-    Type: 'AWS::ApiGateway::BasePathMapping',
-    Properties: {
-      BasePath: '(none)',
-      DomainName: { Ref: 'DomainName' },
-      RestApiId: { Ref: 'PhotoblogUploadServer' },
-      Stage: { Ref: 'Stage' },
-    },
-  };
-
-  cfn.Resources.PhotoblogUploadServer.Properties.EndpointConfiguration = 'EDGE';
 
   return cfn;
 };
